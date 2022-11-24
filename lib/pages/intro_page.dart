fe72dc083993f18bee8ec45f1a80fcca0d0efe69
Erdem <erdemgocen44@gmail.com>
2022-11-25 00:58:19 +0100

    pubspect ayarlandı

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
