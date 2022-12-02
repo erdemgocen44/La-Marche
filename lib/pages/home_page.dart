71b2c4c09c5887348710422a974da0de691e03e7
Erdem <erdemgocen44@gmail.com>
2022-12-02 21:15:57 +0100

    home page commerce

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,19 +11,23 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            SizedBox(
+          children:  [
+            const SizedBox(
               height: 50,
             ),
 
             //bonjour!!
-            Padding(
+            const Padding(
               padding: EdgeInsets.symmetric(
                 horizontal: 25,
               ),
               child: Text(
                 'Bonjour!',
               ),
+            ),
+           
+            const SizedBox(
+              height: 5,
             ),
 
             //Commandons des articles frais pour vous
@@ -31,7 +36,11 @@ class HomePage extends StatelessWidget {
                 horizontal: 25,
               ),
               child: Text(
-                'Commandons des articles frais pour vous',
+                'Commandons des articles frais pour vous',style: GoogleFonts.nerkoOne(
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,,
               ),
             ),
 
