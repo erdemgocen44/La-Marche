dd299551aa610ea34260f0855e38901abdc4f7d8
Erdem <erdemgocen44@gmail.com>
2022-12-03 20:59:35 +0100

    cartmodel created

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -4,8 +4,10 @@ class CartModel extends ChangeNotifier {
   //list of items on sale
 
   final List _shopItems = [
-//itemName, itemPrice,imagePath
-
-
+//itemName, itemPrice,imagePath et color
+    ['Avocat', '4.50', 'assets/images/avocat.png', Colors.greenAccent],
+    ['Bananas', '2.50', 'assets/images/bananas.png', Colors.yellowAccent],
+    ['Poulet', '14.00', 'assets/images/poulet.png', Colors.brown],
+    ['Eau', '0.50', 'assets/images/eau.png', Colors.blueAccent],
   ];
 }
