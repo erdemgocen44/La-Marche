7872685fc4397a93ba76d206f30e8bf19ccea5a8
Erdem <erdemgocen44@gmail.com>
2022-12-03 20:46:47 +0100

    grocery item tile created

--- lib/components/grocery_item_tile.dart
+++ lib/components/grocery_item_tile.dart
@@ -1,7 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class GroceryItemTile extends StatelessWidget {
-  const GroceryItemTile({super.key});
+  final String itemName;
+  final String itemPrice;
+  final String imagePath;
+  final color;
+
+  const GroceryItemTile({
+    Key? key,
+    required this.itemName,
+    required this.itemPrice,
+    required this.imagePath,
+    this.color,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
