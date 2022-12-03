fecd7b9930a5557bbaef9b833790f4bc6031c16f
Erdem <erdemgocen44@gmail.com>
2022-12-03 21:18:14 +0100

    provider fini

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:marche/model/cart_model.dart';
+import 'package:provider/provider.dart';
 
 import '../components/grocery_item_tile.dart';
 
@@ -83,12 +85,19 @@ class HomePage extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                itemBuilder: (context, index) {
-                  return GroceryItemTile();
-                },
+              child: Consumer<CartModel>(
+                builder: (context, value, child) => GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return GroceryItemTile(
+                      itemName: value.shopItems[index][0],
+                      itemPrice: value.shopItems[index][1],
+                      imagePath: value.shopItems[index][2],
+                      color: value.shopItems[index][3],
+                    );
+                  },
+                ),
               ),
             ),
           ],
