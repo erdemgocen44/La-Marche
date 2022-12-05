b29f421a24dcedbd78814f73e862a0a4cf63a2d9
Erdem <erdemgocen44@gmail.com>
2022-12-05 21:40:01 +0100

    cart model updated

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -21,8 +21,23 @@ class CartModel extends ChangeNotifier {
 
 //add item to cart
 
+  void addItemToCart(int index) {
+    _cartItems.add(_shopItems[index]);
+    notifyListeners();
+  }
+
 //remove item from  cart
+  void removeItemFromCart(int index) {
+    _cartItems.removeAt(index);
+    notifyListeners();
 
 //calculatee total price
+    void calculateTotal() {
+      double totalPrice = 0;
 
+      for (var i = 0; i < _cartItems.length; i++) {
+        totalPrice += double.parse(_cartItems[i][1]);
+      }
+    }
+  }
 }
